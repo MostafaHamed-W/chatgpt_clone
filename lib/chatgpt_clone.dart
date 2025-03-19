90c2a5dae2436e2a985487ee1c2b2430bcdebe80
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2025-03-19 20:37:48 +0200

    Add custom appbar design

--- lib/chatgpt_clone.dart
+++ lib/chatgpt_clone.dart
@@ -1,20 +1,25 @@
 import 'package:chatgpt_clone/chat/ui/chat_screen.dart';
 import 'package:chatgpt_clone/core/theming/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChatGPT extends StatelessWidget {
   const ChatGPT({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ChatGPT Clone',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: ColorsManager.scaffoldBackground,
-        brightness: Brightness.dark,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'ChatGPT Clone',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: ColorsManager.scaffoldBackground,
+          brightness: Brightness.dark,
+        ),
+        home: const ChatScreen(),
       ),
-      home: const ChatScreen(),
     );
   }
 }
